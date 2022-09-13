424427d3d382421476b3d64650c060e6bf4e0215
William Diaz Orozco <will.diazor@gmail.com>
2022-09-13 11:23:47 -0500

    Refactoring functions

--- 00-RandomTests.js
+++ 00-RandomTests.js
@@ -206,19 +206,9 @@ arrayDiff([1,2,2,2,3],[2]) == [1,3]
  */
 
 function arrayDiff(a, b) {
-  let counter = 0;
-  let newArr = [];
-  a.forEach(value => {
-    b.forEach( v => {
-      if(value===v) counter++;
-    })
-    if(counter===0) newArr.push(value);
-    counter = 0;
-  })
-
-  return newArr;
+  return a.filter(e => !b.includes(e));
 }
 
 
 
-console.log(arrayDiff([1,2,2],[ 1 ]))
+console.log(arrayDiff([1,2,2,3,3],[ 1 ]))
